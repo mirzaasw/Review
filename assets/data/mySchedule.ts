6a7c94fdadcf6b24e478555c092069d944a9bd99
mirzaasw <52796909+mirzaasw@users.noreply.github.com>
2019-08-09 21:27:37 -0400

    Add files via upload

---
+++ assets/data/mySchedule.ts
@@ -0,0 +1,9 @@
+import { BusSchedule } from '../../app/schedule';
+
+export const SCHEDULE : BusSchedule[]=[
+    {routes:"HERE TO THERE",leave:"soon",arrive:"later"},
+
+    {routes:"There TO HERE",leave:'later', arrive:"soon"}
+
+
+]
